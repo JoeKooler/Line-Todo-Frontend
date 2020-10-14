b046489f3430f84e6643694fb29afa40f1a3ea37
JoeKooler <jojesin@gmail.com>
2020-10-14 19:58:14 +0700

    restroom bug fix

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,31 +1,27 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { LoginAction } from "../../Redux/User/Action";
-
 import liff from "@line/liff";
+// import { useDispatch, useSelector } from "react-redux";
+// import { LoginAction } from "../../Redux/User/Action";
 
 function LiffLogin() {
-  const { loginSuccess, user, error } = useSelector(
-    (state) => state.UserReducer
-  );
+  // const { loginSuccess, user, error } = useSelector(
+  //   (state) => state.UserReducer
+  // );
   const [userName, setUserName] = useState("");
-  const LIFF_ID = "1655100279-x4wN2gY1";
-  const dispatch = useDispatch();
-  const loginAct = (user) => dispatch(LoginAction(user));
+  // const LIFF_ID = "1655100279-x4wN2gY1";
+  // const dispatch = useDispatch();
+  // const loginAct = (user) => dispatch(LoginAction(user));
 
   useEffect(() => {
-    const getLiff = async () => {
+    const LiffInit = async() =>{
       await liff.init({ liffId: LIFF_ID });
-      if (loginSuccess) {
-        await liff.login();
-        loginAct(await liff.getProfile());
-        console.log(user);
-        setUserName((await liff.getProfile()).displayName);
-      } else {
-        setUserName((await liff.getProfile()).displayName);
-      }
-    };
-    getLiff();
+    }
+    LiffInit();
+    if (liff.isLoggedIn()) {
+      setUserName((await liff.getProfile()).displayName);
+    }else {
+      liff.login();
+    }
   }, []);
   return (
     <div className="App">

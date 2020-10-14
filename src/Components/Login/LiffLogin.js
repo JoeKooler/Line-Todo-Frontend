51168485816d763737ad1ed5f9af207ad0dbc0fa
JoeKooler <jojesin@gmail.com>
2020-10-15 00:08:39 +0700

    Fix login action

--- src/Components/Login/LiffLogin.js
+++ src/Components/Login/LiffLogin.js
@@ -1,11 +1,14 @@
 import React, { useState, useEffect } from "react";
 import liff from "@line/liff";
-import { useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { LoginAction } from "../../Redux/User/Action";
 
 function LiffLogin() {
   const LIFF_ID = "1655100279-x4wN2gY1";
+  const { loginSuccess } = useSelector((state) => state.UserReducer);
+
   const dispatch = useDispatch();
-  const setLogin = (user) => dispatch(user);
+  const setLogin = (user) => dispatch(LoginAction(user));
   useEffect(() => {
     const LiffInit = async () => {
       await liff.init({ liffId: LIFF_ID });

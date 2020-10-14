60f44f74c918a501285fd43717dbe0ef55f5aaea
JoeKooler <jojesin@gmail.com>
2020-10-14 20:28:14 +0700

    try hard code

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,20 +1,13 @@
 import React, { useState, useEffect } from "react";
 import liff from "@line/liff";
-// import { useDispatch, useSelector } from "react-redux";
-// import { LoginAction } from "../../Redux/User/Action";
 
 function LiffLogin() {
-  // const { loginSuccess, user, error } = useSelector(
-  //   (state) => state.UserReducer
-  // );
   const [userName, setUserName] = useState("");
   const LIFF_ID = "1655100279-x4wN2gY1";
-  // const dispatch = useDispatch();
-  // const loginAct = (user) => dispatch(LoginAction(user));
 
   useEffect(() => {
     const LiffInit = async () => {
-      await liff.init({ liffId: LIFF_ID });
+      await liff.init({ liffId: "1655100279-x4wN2gY1" });
     };
 
     const setName = async () => {
